8f811867a8f9acfee9a6b25a0f5ee4288d00cc91
edelgarat <edelgarat@yandex.ru>
2021-03-19 16:02:17 +0300

    Обновлен хелпер provideRef

--- src/hooks/libs/provideRef.ts
+++ src/hooks/libs/provideRef.ts
@@ -1,6 +1,6 @@
 import { isFunction } from "@worksolutions/utils";
 
-export function provideRef(...to: ({ current: any } | ((ref: any) => void) | null)[]) {
+export function provideRef(...to: ({ current: any } | ((ref: any) => void) | null | undefined)[]) {
   return function (ref: any) {
     to.forEach((toElement) => {
       if (!toElement) return;
