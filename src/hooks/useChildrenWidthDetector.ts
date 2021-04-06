c591d69c1bbe14251c0c3970f60ea375d8515a9b
edelgarat <edelgarat@yandex.ru>
2021-04-06 12:20:53 +0300

    Обновление хука useChildrenWidthDetector

--- src/hooks/useChildrenWidthDetector.ts
+++ src/hooks/useChildrenWidthDetector.ts
@@ -1,11 +1,22 @@
 import React from "react";
 import { htmlCollectionToArray } from "@worksolutions/utils";
 
-export function useChildrenWidthDetector() {
+export function useChildrenWidthDetector(useResizeObserver = false) {
   const [widths, setWidths] = React.useState<number[] | null>(null);
-  const initRef = React.useCallback((element: HTMLElement | null) => {
-    if (!element) return;
-    setWidths(htmlCollectionToArray(element.children).map((element) => element.getBoundingClientRect().width));
-  }, []);
+  const initRef = React.useCallback(
+    (element: HTMLElement | null) => {
+      if (!element) return;
+      const update = () =>
+        setWidths(htmlCollectionToArray(element.children).map((element) => element.getBoundingClientRect().width));
+
+      if (useResizeObserver) {
+        new ResizeObserver(update).observe(element);
+        return;
+      }
+
+      update();
+    },
+    [useResizeObserver],
+  );
   return { widths, initRef };
 }
