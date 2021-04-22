1650a3642d09754e1063ca8d8a39cffd2b3dc56c
edelgarat <edelgarat@yandex.ru>
2021-04-22 10:42:11 +0300

    Добавлен параметр invokeImmediately в хук useObservableAsState

--- src/hooks/useObservableAsState.ts
+++ src/hooks/useObservableAsState.ts
@@ -1,15 +1,24 @@
 import React from "react";
 import { observe, toJS } from "mobx";
 
-export function useObservableAsState<T extends Record<string, any>, K extends keyof T>(target: T, key: K): T[K] {
+export function useObservableAsState<T extends Record<string, any>, K extends keyof T>(
+  target: T,
+  key: K,
+  invokeImmediately?: boolean,
+): T[K] {
   const [value, setValue] = React.useState(() => target[key]);
 
   React.useEffect(() => {
     let mounted = true;
-    const unmount = observe(target, key, () => {
-      if (!mounted) return;
-      setValue(toJS(target[key]));
-    });
+    const unmount = observe(
+      target,
+      key,
+      () => {
+        if (!mounted) return;
+        setValue(toJS(target[key]));
+      },
+      invokeImmediately,
+    );
     return () => {
       mounted = false;
       unmount();
