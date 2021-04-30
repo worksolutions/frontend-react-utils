84786ea3c7703511db60403a2280038920c7599c
edelgarat <edelgarat@yandex.ru>
2021-04-30 08:28:51 +0300

    Обновление хука useQueryParams

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -1,14 +1,19 @@
 import React from "react";
 import { useLocation, useHistory } from "react-router";
 import qs from "qs";
+import { isDeepEqual, isNotNil } from "@worksolutions/utils";
 
 export function useQueryParams<P extends string>() {
   const { search, pathname } = useLocation();
   const history = useHistory();
   const query = React.useMemo(() => qs.parse(search.slice(1)) as Partial<Record<P, string>>, [search]);
   const setQuery = React.useCallback(
-    (newQuery: Partial<Record<P, string | number>>, replace = false) => {
-      history[replace ? "replace" : "push"](`${pathname}?${qs.stringify({ ...query, ...newQuery })}`);
+    (queryPatch: Partial<Record<P, string | number>>, replace = false) => {
+      const newQuery = Object.fromEntries(
+        Object.entries({ ...query, ...queryPatch }).filter(([, value]) => isNotNil(value)),
+      );
+      if (isDeepEqual(query, newQuery)) return;
+      history[replace ? "replace" : "push"](`${pathname}?${qs.stringify(newQuery)}`);
     },
     [history, pathname, query],
   );
