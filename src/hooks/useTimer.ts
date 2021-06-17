7f19d32bfeecbeee15272913c51c63a10556c71c
edelgarat <edelgarat@yandex.ru>
2021-06-17 13:51:56 +0300

    Обновление хука таймера

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,43 +1,57 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { isNil } from "ramda";
 
 import { useForceUpdate } from "./common";
 
-export function useTimer(data: {
+export function useTimer({
+  finisher,
+  tickHandler,
+  initialValue,
+  interval,
+  onSuccess,
+}: {
   interval: number;
   initialValue: () => number;
-  handler: (value: number) => number;
+  tickHandler: (value: number) => number;
   finisher: (value: number) => boolean;
   onSuccess?: () => void;
 }) {
   const forceUpdate = useForceUpdate();
-  const timerRef = useRef<number>();
+  const timerRef = useRef<NodeJS.Timeout>(null!);
   const valueRef = useRef(0);
 
-  function start(seconds?: number) {
-    clearInterval(timerRef.current);
-    valueRef.current = isNil(seconds) ? data.initialValue() : seconds;
-    forceUpdate();
-    // @ts-ignore
-    timerRef.current = setInterval(() => {
-      valueRef.current = data.handler(valueRef.current);
-      forceUpdate();
-      if (data.finisher(valueRef.current)) {
-        clearInterval(timerRef.current);
-        if (data.onSuccess) data.onSuccess();
+  const start = useCallback(
+    (seconds?: number) => {
+      clearInterval(timerRef.current);
+      if (finisher(valueRef.current)) {
+        if (onSuccess) onSuccess();
+        return;
       }
-    }, data.interval);
-  }
 
-  function stop() {
-    if (data.onSuccess) data.onSuccess();
+      valueRef.current = isNil(seconds) ? initialValue() : seconds;
+      forceUpdate();
+
+      timerRef.current = setInterval(() => {
+        valueRef.current = tickHandler(valueRef.current);
+        forceUpdate();
+        if (finisher(valueRef.current)) {
+          clearInterval(timerRef.current);
+          if (onSuccess) onSuccess();
+        }
+      }, interval) as any;
+    },
+    [finisher, forceUpdate, tickHandler, initialValue, interval, onSuccess],
+  );
+
+  const stop = useCallback(() => {
+    if (onSuccess) onSuccess();
     clearInterval(timerRef.current);
-  }
+  }, [onSuccess]);
 
   useEffect(() => {
-    valueRef.current = data.initialValue();
+    valueRef.current = initialValue();
     return () => clearInterval(timerRef.current);
-  }, []);
+  }, [initialValue]);
 
   return {
     value: valueRef.current,
