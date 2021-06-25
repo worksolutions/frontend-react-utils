048a5c40c58071f3121a7a0ef52bdc739448b0c1
edelgarat <edelgarat@yandex.ru>
2021-06-25 10:13:20 +0300

    Обновление хука useScrollCallback

--- src/hooks/scroll.ts
+++ src/hooks/scroll.ts
@@ -1,23 +1,40 @@
 import React from "react";
 import useMeasureDirty from "react-use/esm/useMeasureDirty";
+import { identity } from "ramda";
 
-export function useScrollCallback(callback: (y: number, x: number) => void, triggerOnElementChange = false) {
-  const element = React.useRef<HTMLElement | null>();
-  const run = () => {
-    if (!element.current) return;
-    if (triggerOnElementChange) callback(0, 0);
-    element.current.addEventListener("scroll", () => {
-      callback(element.current!.scrollTop, element.current!.scrollLeft);
-    });
-  };
-
-  return (el: HTMLElement | null) => {
-    element.current = el;
-    run();
-  };
+function getWindowScrollPosition() {
+  return { x: document.documentElement.scrollLeft, y: document.documentElement.scrollTop };
 }
 
-export function useScrollCallbackWasScrolledBoolean() {
+export function useScrollCallback(callback: (y: number, x: number) => void, triggerOnElementChange = false) {
+  const disposeRef = React.useRef<Function>();
+  const runListener = (element: HTMLElement | Window | null) => {
+    if (!element) return identity;
+    if (triggerOnElementChange) callback(0, 0);
+
+    const scrollHandler =
+      element === window
+        ? () => {
+            const { x, y } = getWindowScrollPosition();
+            callback(y, x);
+          }
+        : () => {
+            callback((element as HTMLElement).scrollTop, (element as HTMLElement).scrollLeft);
+          };
+
+    element.addEventListener("scroll", scrollHandler);
+    return () => element.removeEventListener("scroll", scrollHandler);
+  };
+
+  React.useEffect(() => () => disposeRef.current && disposeRef.current(), []);
+
+  return React.useCallback((el: HTMLElement | Window | null) => {
+    if (disposeRef.current) disposeRef.current();
+    disposeRef.current = runListener(el);
+  }, []);
+}
+
+export function useScrollOnStart() {
   const [scrolled, setScrolled] = React.useState(false);
   const setScrollableElement = useScrollCallback((y) => {
     if (y === 0) {
@@ -28,7 +45,7 @@ export function useScrollCallbackWasScrolledBoolean() {
     setTimeout(setScrolled, 1, true);
   });
 
-  return { setScrollableElement, scrolled };
+  return [scrolled, setScrollableElement] as const;
 }
 
 export function useScrollToElement(center: boolean, behavior?: ScrollBehavior, position = "relative") {
