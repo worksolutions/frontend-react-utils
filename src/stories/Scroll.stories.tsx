0af8f0a6c4130c5bf5f4a5093c2cd735fe819de5
Дмитрий Переверза <d.pereverza@worksolutions.ru>
2021-07-18 13:47:50 +0300

    add StoryBook

---
+++ src/stories/Scroll.stories.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { times } from "ramda";
+import { useVerticalScrollInfo } from "../hooks";
+
+const Demo = () => {
+  const [result, setResult] = useState({});
+  const init = useVerticalScrollInfo(setResult);
+  return (
+    <div>
+      <div
+        ref={init}
+        style={{
+          overflowY: "scroll",
+          height: "100px",
+          border: "1px solid red",
+        }}
+      >
+        {times(
+          (i) => (
+            <div>{i}</div>
+          ),
+          30,
+        )}
+      </div>
+      <br />
+      <br />
+      <div>Result: {JSON.stringify(result)}</div>
+    </div>
+  );
+};
+
+export default {
+  title: "Hooks",
+  component: Demo,
+} as ComponentMeta<typeof Demo>;
+
+const Template: ComponentStory<typeof Demo> = () => <Demo />;
+
+export const UseVerticalScrollInfo = Template.bind({});
+UseVerticalScrollInfo.storyName = "useVerticalScrollInfo";
