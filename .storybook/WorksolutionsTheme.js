03f2da405a7156afdbf13c596b50f9785862a977
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2021-08-18 22:24:41 +0300

    Добавлены темы для storybook

---
+++ .storybook/WorksolutionsTheme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "Worksolutions",
+  brandUrl: "https://worksolutions.ru/",
+  brandImage: "https://worksolutions.ru/i/logo_ws.svg",
+});
