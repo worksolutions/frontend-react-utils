0986240536e5b866b9dd38e73d6fa8a192d3a204
edelgarat <edelgarat@yandex.ru>
2021-09-17 12:47:16 +0300

    update useChildrenMeasure hook: add relative measures

--- src/hooks/useChildrenMeasure.ts
+++ src/hooks/useChildrenMeasure.ts
@@ -3,12 +3,33 @@ import { htmlCollectionToArray } from "@worksolutions/utils";
 
 export function useChildrenMeasure(useResizeObserver = false) {
   const [measures, setMeasures] = React.useState<DOMRect[] | null>(null);
+  const [relativeMeasures, setRelativeMeasures] = React.useState<DOMRect[] | null>(null);
   const elementRef = React.useRef<HTMLElement | null>(null);
 
   const update = React.useCallback(() => {
     if (!elementRef.current) return;
-    const childrenArray = htmlCollectionToArray(elementRef.current.children);
-    setMeasures(childrenArray.map((element) => element.getBoundingClientRect()));
+    const childrenRects = htmlCollectionToArray(elementRef.current.children).map((element) =>
+      element.getBoundingClientRect(),
+    );
+    const parentRect = elementRef.current.getBoundingClientRect();
+    setMeasures(childrenRects);
+    setRelativeMeasures(
+      childrenRects.map((childrenRect) => {
+        const x = childrenRect.x - parentRect.x;
+        const y = childrenRect.y - parentRect.y;
+        return {
+          toJSON: () => "",
+          width: childrenRect.width,
+          height: childrenRect.height,
+          x,
+          y,
+          left: x,
+          top: y,
+          bottom: y + childrenRect.height,
+          right: x + childrenRect.width,
+        };
+      }),
+    );
   }, []);
 
   const initRef = React.useCallback(
@@ -23,7 +44,7 @@ export function useChildrenMeasure(useResizeObserver = false) {
 
       update();
     },
-    [useResizeObserver],
+    [update, useResizeObserver],
   );
-  return { measures, initRef, update };
+  return { measures, relativeMeasures, initRef, update };
 }
