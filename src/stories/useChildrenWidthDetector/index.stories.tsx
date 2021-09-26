b4b9d282a55908829683f9c11d2d15494e36b194
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2021-09-26 10:33:41 +0300

    добавлен сторибук для хука useChildrenWidthDetector

---
+++ src/stories/useChildrenWidthDetector/index.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import ForceUnmountingComponent from "../../utils/storyHelpers/ForceUnmountingComponent";
+import DebugComponent from "../../utils/storyHelpers/DebugComponent";
+import { booleanControl } from "../../utils/storyHelpers/controls";
+
+import { useChildrenWidthDetector } from "../../hooks";
+
+const defaultElements = () => new Array(5).fill(null);
+
+const Demo = (props: { useResizeObserver: boolean }) => {
+  const { update, initRef, widths } = useChildrenWidthDetector(props.useResizeObserver);
+  console.log(widths);
+
+  return (
+    <div>
+      <div ref={initRef}>
+        {defaultElements().map((_, index) => (
+          <div>item {index}</div>
+        ))}
+      </div>
+      <DebugComponent data={widths} />
+      <div onClick={update} style={{ marginTop: 20, cursor: "pointer", fontWeight: 700 }}>
+        Update widths
+      </div>
+    </div>
+  );
+};
+
+const Template: ComponentStory<typeof Demo> = (props) => {
+  return (
+    <ForceUnmountingComponent>
+      <Demo {...props} />
+    </ForceUnmountingComponent>
+  );
+};
+
+export default {
+  title: "Hooks/useChildrenWidthDetector",
+  component: Demo,
+  argTypes: {
+    useResizeObserver: {
+      name: "Использовать ResizeObserver",
+      defaultValue: true,
+      ...booleanControl(),
+    },
+  },
+} as ComponentMeta<typeof Demo>;
+
+export const useChildrenWidthDetectorInfo = Template.bind({});
+useChildrenWidthDetectorInfo.args = {};
+
+useChildrenWidthDetectorInfo.storyName = "useChildrenWidthDetector";
