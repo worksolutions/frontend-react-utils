ba4abba93c6879824326fbde5e88ddb269674720
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2021-10-05 00:07:46 +0300

    Добавлено отключение resizeObserver если компонент удалился

--- src/hooks/useChildrenMeasure.ts
+++ src/hooks/useChildrenMeasure.ts
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { htmlCollectionToArray } from "@worksolutions/utils";
 
+export function computeRelativeMeasures(childrenRects: DOMRect[], parentRect: DOMRect) {
+  return childrenRects.map((childrenRect) => {
+    const x = childrenRect.x - parentRect.x;
+    const y = childrenRect.y - parentRect.y;
+
+    return {
+      toJSON: () => "",
+      width: childrenRect.width,
+      height: childrenRect.height,
+      x,
+      y,
+      left: x,
+      top: y,
+      bottom: y + childrenRect.height,
+      right: x + childrenRect.width,
+    };
+  });
+}
+
 export function useChildrenMeasure(useResizeObserver = false) {
+  const elementRef = React.useRef<HTMLElement | null>(null);
+  const resizeObserverRef = React.useRef<ResizeObserver>(null!);
+
   const [measures, setMeasures] = React.useState<DOMRect[] | null>(null);
   const [relativeMeasures, setRelativeMeasures] = React.useState<DOMRect[] | null>(null);
-  const elementRef = React.useRef<HTMLElement | null>(null);
 
   const update = React.useCallback(() => {
     if (!elementRef.current) return;
     const childrenRects = htmlCollectionToArray(elementRef.current.children).map((element) =>
       element.getBoundingClientRect(),
     );
+
     const parentRect = elementRef.current.getBoundingClientRect();
     setMeasures(childrenRects);
-    setRelativeMeasures(
-      childrenRects.map((childrenRect) => {
-        const x = childrenRect.x - parentRect.x;
-        const y = childrenRect.y - parentRect.y;
-        return {
-          toJSON: () => "",
-          width: childrenRect.width,
-          height: childrenRect.height,
-          x,
-          y,
-          left: x,
-          top: y,
-          bottom: y + childrenRect.height,
-          right: x + childrenRect.width,
-        };
-      }),
-    );
+    setRelativeMeasures(computeRelativeMeasures(childrenRects, parentRect));
   }, []);
 
   const initRef = React.useCallback(
@@ -38,7 +44,7 @@ export function useChildrenMeasure(useResizeObserver = false) {
       if (!element) return;
 
       if (useResizeObserver) {
-        new ResizeObserver(update).observe(element);
+        resizeObserverRef.current = new ResizeObserver(update);
         return;
       }
 
@@ -46,5 +52,10 @@ export function useChildrenMeasure(useResizeObserver = false) {
     },
     [update, useResizeObserver],
   );
+
+  useEffect(() => {
+    return () => resizeObserverRef.current && resizeObserverRef.current.disconnect();
+  });
+
   return { measures, relativeMeasures, initRef, update };
 }
