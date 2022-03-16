274322cec5f60e7f617c6ec0459170d388a7379f
edelgarat <edelgarat@yandex.ru>
2022-03-16 16:25:52 +0300

    update dependency versions and remove storybook

--- .storybook/WorksolutionsTheme.js
+++
@@ -1,8 +0,0 @@
-import { create } from "@storybook/theming";
-
-export default create({
-  base: "light",
-  brandTitle: "Worksolutions",
-  brandUrl: "https://worksolutions.ru/",
-  brandImage: "https://worksolutions.ru/i/logo_ws.svg",
-});
--- .storybook/manager.js
+++
@@ -1,4 +0,0 @@
-import { addons } from "@storybook/addons";
-import WorksolutionsTheme from "./WorksolutionsTheme";
-
-addons.setConfig({ theme: WorksolutionsTheme });
