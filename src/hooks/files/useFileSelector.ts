8c2c56f14eabe21e37e99553a0a5404ddd06d33e
edelgarat <edelgarat@yandex.ru>
2022-04-02 10:46:54 +0300

    update useFileSelector hook

--- src/hooks/files/useFileSelector.ts
+++ src/hooks/files/useFileSelector.ts
@@ -2,8 +2,27 @@ import React from "react";
 import { useDropArea } from "react-use";
 import { AcceptTypes, convertFileToFileInterface, createFileInput, FileInterface } from "@worksolutions/utils";
 
-export function useFileSelector(onChange: (file: FileInterface) => void, acceptTypes?: AcceptTypes[]) {
-  const handleChange = React.useCallback(([file]: File[]) => onChange(convertFileToFileInterface(file)), [onChange]);
+type Result = { dropAreaProps: ReturnType<typeof useDropArea>[0]; dropping: boolean; openNativeFileDialog: () => void };
+
+export function useFileSelector(
+  onChange: (files: FileInterface[]) => void,
+  options: { acceptTypes?: AcceptTypes[]; multiply: true },
+): Result;
+
+export function useFileSelector(
+  onChange: (files: FileInterface) => void,
+  options: { acceptTypes?: AcceptTypes[]; multiply: false },
+): Result;
+
+export function useFileSelector(
+  onChange: ((files: FileInterface) => void) | ((files: FileInterface[]) => void),
+  { acceptTypes, multiply }: { acceptTypes?: AcceptTypes[]; multiply: boolean },
+) {
+  const handleChange = React.useCallback(
+    (files: File[]) =>
+      onChange(multiply ? files.map(convertFileToFileInterface) : (convertFileToFileInterface(files[0]) as any)),
+    [onChange],
+  );
   const input = React.useMemo(() => createFileInput(handleChange, false, acceptTypes), [handleChange, acceptTypes]);
   const [dropAreaProps, dropAreaState] = useDropArea({ onFiles: handleChange });
 
