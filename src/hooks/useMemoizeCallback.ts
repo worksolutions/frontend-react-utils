78ec8f6d5e62de7d3854aec73af69e61ea0c27ea
edelgarat <edelgarat@yandex.ru>
2022-11-18 10:54:46 +0300

    update useMemoizeCallback hook for eslint correct hook dependencies detection

--- src/hooks/useMemoizeCallback.ts
+++ src/hooks/useMemoizeCallback.ts
@@ -3,20 +3,20 @@ import { memoizeWith } from "ramda";
 
 /**
  * Memoize callback in React.useCallback hook
- * @param memoizeWithParam - function to detect changes: => key for memoize
  * @param callback - useCallback hook callback
  * @param deps - useCallback hook dependencies
+ * @param memoizeWithParam - function to detect changes: => key for memoize
  */
 export function useMemoizeCallback<ARGS extends any[], RESULT>(
-  memoizeWithParam: null | ((...args: ARGS) => any),
   callback: (...args: ARGS) => RESULT,
-  deps: DependencyList = [],
+  deps: DependencyList,
+  memoizeWithParam: (...args: ARGS) => any,
 ): (...args: ARGS) => RESULT {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoizedCallback = React.useCallback(callback, deps);
 
   return React.useMemo(
-    () => memoizeWith((memoizeWithParam || (() => null)) as any, memoizedCallback as any),
+    () => memoizeWith(memoizeWithParam as any, memoizedCallback as any),
     [memoizeWithParam, memoizedCallback],
   );
 }
