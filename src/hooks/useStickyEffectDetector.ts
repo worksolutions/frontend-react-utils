15e48df9fe7cd15ee9f0220f7bdb7bdc7b9f58d1
edelgarat <edelgarat@yandex.ru>
2023-07-07 11:53:12 +0300

    update sticky effect detector

--- src/hooks/useStickyEffectDetector.ts
+++ src/hooks/useStickyEffectDetector.ts
@@ -3,10 +3,13 @@ import React from "react";
 import { useEffectSkipFirst } from "./common";
 import { useSyncToRef } from "./useSyncToRef";
 
-export function useStickyEffectDetector(onChangeStickyEffect: (sticky: boolean) => void) {
+export function useStickyEffectDetector(onChangeStickyEffect: (sticky: boolean) => void, rootMargin?: string) {
   const [observer, setObserver] = React.useState<IntersectionObserver>();
   const onChangeStickyEffectRef = useSyncToRef(onChangeStickyEffect);
-  React.useEffect(() => setObserver(createObserver(onChangeStickyEffectRef)), [onChangeStickyEffectRef]);
+  React.useEffect(
+    () => setObserver(createObserver(onChangeStickyEffectRef, rootMargin)),
+    [onChangeStickyEffectRef, rootMargin],
+  );
 
   useEffectSkipFirst(() => () => observer?.disconnect(), [observer]);
 
@@ -21,12 +24,12 @@ export function useStickyEffectDetector(onChangeStickyEffect: (sticky: boolean) 
   return setObservableElement;
 }
 
-function createObserver(callbackRef: React.MutableRefObject<(sticky: boolean) => void>) {
+function createObserver(callbackRef: React.MutableRefObject<(sticky: boolean) => void>, rootMargin?: string) {
   return new IntersectionObserver(
     ([entry]) => {
       if (entry.boundingClientRect.width === 0 || entry.boundingClientRect.height === 0) return;
       callbackRef.current(entry.intersectionRatio < 1);
     },
-    { threshold: [1] },
+    { threshold: [1], rootMargin },
   );
 }
