ee0ff263c31750a4491286da74453394819408db
edelgarat <edelgarat@yandex.ru>
2024-03-14 11:38:13 +0300

    update usePrevious hook

--- src/hooks/common.ts
+++ src/hooks/common.ts
@@ -38,8 +38,6 @@ export function useEffectSkipFirst(callback: React.EffectCallback, dependencies?
 
 export function usePrevious<T>(value: T) {
   const ref = useRef<T>(value);
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
+  useMemo(() => void (ref.current = value), [value]);
   return ref.current;
 }
